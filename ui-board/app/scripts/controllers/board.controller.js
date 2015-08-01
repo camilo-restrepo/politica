2c1dd7d0e10d566c5d0d3fdbc15d7b328950b692
Julian Espinel <julianespinel@gmail.com>
2015-07-31 23:58:35 -0500

    Improve ui board and cards. Add polarity labels.

--- ui-board/app/scripts/controllers/board.controller.js
+++ ui-board/app/scripts/controllers/board.controller.js
@@ -4,57 +4,76 @@ boardModule.controller('boardController', boardController);
 boardController.$inject = ['$scope', '$websocket', 'TargetsService'];
 
 function boardController($scope, $websocket, TargetsService) {
-	$scope.candidatos = [];
+  $scope.candidatos = [];
 
-    var ws = $websocket.$new({
-		url: 'ws://localhost:9001/board/api/ws',
-		protocols: []
-	}); 
+  var ws = $websocket.$new({
+    url: 'ws://localhost:9001/board/api/ws',
+    protocols: []
+  }); 
 
-    ws.$on('$open', function () {
-    	console.log('Oh my gosh, websocket is really open! Fukken awesome!');
-    });
+  ws.$on('$open', function () {
+    console.log('Oh my gosh, websocket is really open! Fukken awesome!');
+  });
 
-    ws.$on('$message', function (data) {
-       	pushData(data);
-       	//ws.$close();
-    });
+  ws.$on('$message', function (data) {
+    pushData(data);
+    //ws.$close();
+  });
 
-    ws.$on('$close', function () {
-       	console.log('Noooooooooou, I want to have more fun with ngWebsocket, damn it!');
-    });
+  ws.$on('$close', function () {
+    console.log('Noooooooooou, I want to have more fun with ngWebsocket, damn it!');
+  });
 
-    ws.$open();
+  ws.$open();
 
-    function pushData(data){
-		var tweet = data;
-		console.log(tweet);
-		for(var i = 0; i < $scope.candidatos.length; i++){
-			if($scope.candidatos[i].twitterId.id === tweet.targets[0]){
-				$scope.candidatos[i].tweets.push(tweet);
-				if($scope.candidatos[i].tweets.length > 5){
-					$scope.candidatos[i].tweets.shift();
-				}
-			}
-		}
-		$scope.$apply();
+  function compareTweets(tweet1, tweet2) {
+
+    return (tweet1.text === tweet2.text) && (tweet1.userId === tweet2.userId); 
+  }
+
+  function tweetIsInList(tweet, tweetList) {
+
+    var isInList = false;
+
+    for (var i = 0; i < tweetList.length && !isInList; i++) {
+
+      isInList = compareTweets(tweet, tweetList[i]);
     }
 
-    function onError(data){
-    	console.log(data);
+    return isInList;
+  }
+
+  function pushData(data){
+    var tweet = data;
+    console.log(tweet);
+    for(var i = 0; i < $scope.candidatos.length; i++){
+      if($scope.candidatos[i].twitterId.id === tweet.targets[0]){
+        if (!tweetIsInList(tweet, $scope.candidatos[i].tweets)) {
+          $scope.candidatos[i].tweets.push(tweet);
+          if($scope.candidatos[i].tweets.length > 5){
+            $scope.candidatos[i].tweets.shift();
+          }
+        }
+      }
+    }
+    $scope.$apply();
+  }
+
+  function onError(data){
+    console.log(data);
+  }
+
+  function onSuccess(data){
+    for (var i = 0; i < data.length; i++) {
+      $scope.candidatos.push(data[i]);
     }
 
-    function onSuccess(data){
-    	for (var i = 0; i < data.length; i++) {
-        	$scope.candidatos.push(data[i]);
-      	}
-
-      	for (i = 0; i < $scope.candidatos.length; i++) {
-        	$scope.candidatos[i].tweets = [];
-      	}
+    for (i = 0; i < $scope.candidatos.length; i++) {
+      $scope.candidatos[i].tweets = [];
     }
+  }
 
-    $scope.init = function(){
-    	TargetsService.getTargets(onSuccess, onError);
-    };
+  $scope.init = function(){
+    TargetsService.getTargets(onSuccess, onError);
+  };
 }
