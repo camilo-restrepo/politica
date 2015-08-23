601a62dcaa3260c81f807d3de482984801cdbc05
Julian Espinel <julianespinel@gmail.com>
2015-08-23 00:06:49 -0500

    Retrieve cloud tag information from the server.

---
+++ ui-board/app/scripts/services/cloudtag.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+boardModule.factory('cloudtagService', cloudtagService);
+cloudtagService.$inject = ['$resource'];
+
+function cloudtagService($resource) {
+
+  var url = 'http://104.236.26.163:9001/board/api/words';
+  var defaultParams = {};
+
+  var actions = {
+    getAllCandidatesCloudTags: { method: 'GET', url: url, isArray: true }
+  };
+
+  return $resource(url, defaultParams, actions);
+}
