55841635206219eec1d7dfbe4621a81facc601e4
elpisco <elpisco@gmail.com>
2015-08-29 00:13:42 -0500

    Added some stopwords.

--- twitterStreamCrawler/src/main/java/co/inc/twitterStreamCrawler/utils/constants/GlobalConstants.java
+++ twitterStreamCrawler/src/main/java/co/inc/twitterStreamCrawler/utils/constants/GlobalConstants.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class GlobalConstants {
 	public final static String BOARD_URL = "http://localhost:9001/board/api/broadcast";
-	public static final Pattern UNDESIRABLES = Pattern.compile("[\\d+\\]\\[\\+(){},.:;¡!¿?<>%\\.\u2026\r\n\"\\|]");
+	public static final Pattern UNDESIRABLES = Pattern.compile("[\\d+\\]\\[\\+(){},.:;¡!¿?/<>%\\.\u2026\r\n\"\\|]");
 	public static final Pattern SPACE = Pattern.compile(" +");
 
 }
