05f7b965b5dec5b4b9a097b2da12690644ba63aa
Julian Espinel <julianespinel@gmail.com>
2015-10-15 12:29:56 -0400

    IP Change

--- twitterStreamCrawler/src/main/java/co/inc/twitterStreamCrawler/utils/constants/GlobalConstants.java
+++ twitterStreamCrawler/src/main/java/co/inc/twitterStreamCrawler/utils/constants/GlobalConstants.java
@@ -3,7 +3,7 @@ package co.inc.twitterStreamCrawler.utils.constants;
 import java.util.regex.Pattern;
 
 public class GlobalConstants {
-	public final static String BOARD_URL = "http://localhost:9001/board/api/broadcast";
+	public final static String BOARD_URL = "http://10.132.157.190:9001/board/api/broadcast";
 	public static final Pattern UNDESIRABLES = Pattern.compile("[\\d+\\]\\[\\+(){},.;¡!¿“”?/\\-<>%\r\n\\|\\.,;:\u2026►\"]");
 	public static final Pattern SPACE = Pattern.compile(" +");
 
