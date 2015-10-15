76900fbbc742075c810e9e46249071cc5a44aa4f
Julian Espinel <julianespinel@gmail.com>
2015-10-14 22:06:11 -0500

    Add template to select the city.

--- ui-board/app/scripts/router.js
+++ ui-board/app/scripts/router.js
@@ -2,26 +2,31 @@
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/select');
 
-    $stateProvider.state('home', {
-        url: '/home',
-        templateUrl: 'views/board.html'
+    $stateProvider.state('select', {
+      url: '/select',
+      templateUrl: 'views/select.html'
     })
-    
+
+    .state('city', {
+      url: '/city/:cityId',
+      templateUrl: 'views/board.html'
+    })
+
     .state('candidate', {
       url: '/candidatos/:twitterId',
       templateUrl: 'views/candidate.html'
     })
 
     .state('polarity', {
-    	url: '/polaridad/:prediction',
-    	templateUrl: 'views/polaridad.html'
+      url: '/polaridad/:prediction',
+      templateUrl: 'views/polaridad.html'
     })
-    
+
     .state('about', {
-        url: '/about',
-        controller: 'aboutController',
-        templateUrl: 'views/about.html'
+      url: '/about',
+      controller: 'aboutController',
+      templateUrl: 'views/about.html'
     });
 });
